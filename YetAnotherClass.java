37ed58fef33258cdacae32260b878f6d6a47498a
Logesh R <logesh45@gmail.com>
2017-10-10 18:02:45 +0530

    Third Commit

--- YetAnotherClass.java
+++ YetAnotherClass.java
@@ -4,9 +4,6 @@ package com.logeshr.myapplication;
  * Created by logeshr on 10/10/17.
  */
 
-public class AnotherClass {
-
-//    Edit 1
-
-    // Edit 2
+public class YetAnotherClass {
+//    Edit 2
 }
